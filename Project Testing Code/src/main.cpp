f7f95b6184eb8c0cc2922f46de2553841110103c
João Valente <jnvalente@ua.pt>
2023-02-11 15:52:57 +0000

    Initial commit

---
+++ Project Testing Code/src/main.cpp
@@ -0,0 +1,63 @@
+// main.cpp
+// *************************************
+//                #include
+// *************************************
+#include <Arduino.h>
+#include <WiFi.h>
+#include <painlessMesh.h>
+
+// Replace with your network credentials
+#define MESH_SSID "myNetwork"
+#define MESH_PASSWORD "myPassword"
+#define MESH_PORT 5555
+
+Scheduler userScheduler; // to control your personal task
+painlessMesh mesh;
+
+void setup()
+{
+
+  // Intro setup()
+  Serial.begin(115200);
+
+  mesh.init(MESH_SSID, MESH_PASSWORD, &userScheduler, MESH_PORT);
+}
+
+void loop()
+{
+  mesh.update();
+  // *************************************
+  //                WiFi
+  // *************************************
+
+  // Scan nearby Networks
+  int numSSID = WiFi.scanNetworks();
+
+  // print the network found:
+  for (int thisNet = 0; thisNet < numSSID; thisNet++)
+  {
+    // List of ESP32 MAC addresses
+    String MAC_address[] = {
+        "78:E3:6D:18:FE:68",
+        "4C:11:AE:CA:92:91",
+        "78:E3:6D:0A:23:01",
+        "78:21:84:8C:B9:39"};
+    // Length of the array MAC_address
+    int len_MAC_address = sizeof(MAC_address) / sizeof(MAC_address[0]);
+
+    for (int ii = 0; ii < len_MAC_address; ii++)
+    {
+      // Prints the MAC_addresses and the RSSI
+      // that match the ESP32.
+      if (WiFi.BSSIDstr(thisNet) == MAC_address[ii])
+      {
+        Serial.print("MAC Address: ");
+        Serial.print(WiFi.BSSIDstr(thisNet));
+        Serial.print(" | ");
+        Serial.print(" Signal = ");
+        Serial.print(WiFi.RSSI(thisNet));
+        Serial.println("dB");
+      }
+    }
+  }
+}
