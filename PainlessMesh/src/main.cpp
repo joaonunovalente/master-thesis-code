b39fd76ab507c6fc324d7bea3e1f5012c73badfe
João Valente <jnvalente@ua.pt>
2023-02-19 15:12:43 +0000

    New projects created
    More to come

--- PainlessMesh/src/main.cpp
+++ PainlessMesh/src/main.cpp
@@ -21,22 +21,16 @@
 #define BLINK_PERIOD 3000  // milliseconds until cycle repeat
 #define BLINK_DURATION 100 // milliseconds LED is on for
 
-//************************************************************
-//               Network Credentials
-//************************************************************
-
 #define MESH_SSID "myNetwork"
 #define MESH_PASSWORD "myPassword"
 #define MESH_PORT 5555
 
-//************************************************************
-//               Prototypes
-//************************************************************
-// void receivedCallback(uint32_t from, String &msg);
-// void newConnectionCallback(uint32_t nodeId);
-// void changedConnectionCallback();
-// void nodeTimeAdjustedCallback(int32_t offset);
-// void delayReceivedCallback(uint32_t from, int32_t delay);
+// Prototypes
+void receivedCallback(uint32_t from, String &msg);
+void newConnectionCallback(uint32_t nodeId);
+void changedConnectionCallback();
+void nodeTimeAdjustedCallback(int32_t offset);
+void delayReceivedCallback(uint32_t from, int32_t delay);
 
 Scheduler userScheduler; // to control your personal task
 painlessMesh mesh;
@@ -44,70 +38,51 @@ painlessMesh mesh;
 bool calc_delay = false;
 SimpleList<uint32_t> nodes;
 
-//************************************************************
-//               Task Invocation
-//************************************************************
-
-// Task to blink the number of nodes
 void sendMessage();                                                // Prototype
 Task taskSendMessage(TASK_SECOND * 1, TASK_FOREVER, &sendMessage); // start with a one second interval
 
 // Task to blink the number of nodes
-Task blinkNoNodes;
+void functionBlinkNoNodes();
+Task blinkNoNodes(TASK_SECOND * 1, TASK_FOREVER, &functionBlinkNoNodes);
 bool onFlag = false;
 
-//************************************************************
-//              void setup()
-//************************************************************
+// Task Scan Networks
+void functionScanNetworks();
+Task tasksScanNetworks(TASK_SECOND * 10, TASK_FOREVER, &functionScanNetworks);
+
 void setup()
 {
   Serial.begin(115200);
 
   pinMode(LED, OUTPUT);
 
-  // mesh.setDebugMsgTypes(ERROR | DEBUG); // set before init() so that you can see error messages
+  mesh.setDebugMsgTypes(ERROR | STARTUP);
 
   mesh.init(MESH_SSID, MESH_PASSWORD, &userScheduler, MESH_PORT);
-  // mesh.onReceive(&receivedCallback);
-  // mesh.onNewConnection(&newConnectionCallback);
-  // mesh.onChangedConnections(&changedConnectionCallback);
-  // mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
-  // mesh.onNodeDelayReceived(&delayReceivedCallback);
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+  mesh.onNodeDelayReceived(&delayReceivedCallback);
 
-  // --> Task SendMessage
+  userScheduler.addTask(tasksScanNetworks);
+  tasksScanNetworks.enable();
+
   // userScheduler.addTask(taskSendMessage);
   // taskSendMessage.enable();
 
-  // --> Task NumberOfBlinks
-  blinkNoNodes.set(BLINK_PERIOD, (mesh.getNodeList().size() + 1) * 2, []()
-                   {
-      // If on, switch off, else switch on
-      if (onFlag)
-        onFlag = false;
-      else
-        onFlag = true;
-      blinkNoNodes.delay(BLINK_DURATION);
-
-      if (blinkNoNodes.isLastIteration()) {
-        // Finished blinking. Reset task for next run 
-        // blink number of nodes (including this node) times
-        blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
-        // Calculate delay based on current mesh time and BLINK_PERIOD
-        // This results in blinks between nodes being synced
-        blinkNoNodes.enableDelayed(BLINK_PERIOD - 
-            (mesh.getNodeTime() % (BLINK_PERIOD*1000))/1000);
-      } });
-  userScheduler.addTask(blinkNoNodes);
-  blinkNoNodes.enable();
-
-  
+  // userScheduler.addTask(blinkNoNodes);
+  // blinkNoNodes.enable();
 }
 
 void loop()
 {
   mesh.update();
   digitalWrite(LED, !onFlag);
+}
 
+void functionScanNetworks()
+{
   // Scan nearby Networks
   int numSSID = WiFi.scanNetworks();
 
@@ -140,6 +115,27 @@ void loop()
   }
 }
 
+void functionBlinkNoNodes()
+{
+  blinkNoNodes.set(BLINK_PERIOD, (mesh.getNodeList().size() + 1) * 2, []()
+                   {
+      // If on, switch off, else switch on
+      if (onFlag)
+        onFlag = false;
+      else
+        onFlag = true;
+      blinkNoNodes.delay(BLINK_DURATION);
+
+      if (blinkNoNodes.isLastIteration()) {
+        // Finished blinking. Reset task for next run 
+        // blink number of nodes (including this node) times
+        blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
+        // Calculate delay based on current mesh time and BLINK_PERIOD
+        // This results in blinks between nodes being synced
+        blinkNoNodes.enableDelayed(BLINK_PERIOD - 
+            (mesh.getNodeTime() % (BLINK_PERIOD*1000))/1000);
+      } });
+}
 void sendMessage()
 {
   String msg = "Hello from node ";
@@ -158,14 +154,14 @@ void sendMessage()
     calc_delay = false;
   }
 
-  Serial.printf("Sending message: %s\n", msg.c_str());
+  // Serial.printf("Sending message: %s\n", msg.c_str());
 
-  // taskSendMessage.setInterval( random(TASK_SECOND * 1, TASK_SECOND * 5));  // between 1 and 5 seconds
+  taskSendMessage.setInterval(TASK_SECOND * 5); // between 1 and 5 seconds
 }
 
 void receivedCallback(uint32_t from, String &msg)
 {
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+  // Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
 }
 
 void newConnectionCallback(uint32_t nodeId)
@@ -174,6 +170,9 @@ void newConnectionCallback(uint32_t nodeId)
   onFlag = false;
   blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
   blinkNoNodes.enableDelayed(BLINK_PERIOD - (mesh.getNodeTime() % (BLINK_PERIOD * 1000)) / 1000);
+
+  // Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+  // Serial.printf("--> startHere: New Connection, %s\n", mesh.subConnectionJson(true).c_str());
 }
 
 void changedConnectionCallback()
@@ -190,7 +189,8 @@ void changedConnectionCallback()
   // Serial.printf("Connection list:");
 
   // SimpleList<uint32_t>::iterator node = nodes.begin();
-  // while (node != nodes.end()) {
+  // while (node != nodes.end())
+  // {
   //   Serial.printf(" %u", *node);
   //   node++;
   // }
